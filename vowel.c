616b9f94b351365b4b6683b975a6c217f5615a49
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-22 16:27:32 +0530

    vowel
    program

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.d>
+void main()
+{
+char ch;
+printf("\n enter the character");
+scanf("%c",&ch);
+if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='i')||(ch=='I')||(ch=='o')||(ch=='O')||(ch=='u')||(ch=='U'))
+printf("\n the alphabet is a vowel");
+else
+printf("\n the alphabet is not a vowel");
+getch();
+}
